219e5a78db471328413e0e3264c41e5a5fd96071
Aaronhdez <aaronhdezalv@hotmail.com>
2022-07-15 19:19:21 +0100

    second commit: Controller interfaces added

---
+++ Assets/Scripts/Characters/CharacterController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICharacterController {
+    public void Move();
+    public void Attack();
+    public void Die();
+}
---
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour, ICharacterController {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Attack() {
+        throw new System.NotImplementedException();
+    }
+
+    public void Die() {
+        throw new System.NotImplementedException();
+    }
+
+    public void Move() {
+        throw new System.NotImplementedException();
+    }
+}
