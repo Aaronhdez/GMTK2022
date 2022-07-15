552c4203fb649637ef014ced1544e96c6c01508a
luiss <luiss@DESKTOP-7S4BC9I>
2022-07-15 21:17:24 +0100

    Player Controller Update

--- Assets/Scripts/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -13,8 +13,12 @@ public class PlayerController : MonoBehaviour, ICharacterController {
     // Update is called once per frame
     void Update()
     {
-
+        Move();
     }
+
+    float movementSpeed = 5f; //Placeholder
+    float deltaTimeVar = 60f; //placeHolder
+    
     public void Attack() {
         throw new System.NotImplementedException();
     }
@@ -24,6 +28,38 @@ public class PlayerController : MonoBehaviour, ICharacterController {
     }
 
     public void Move() {
-        throw new System.NotImplementedException();
+
+
+        // convert mouse position into world coordinates
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // set vector of transform directly
+        transform.up = mouseScreenPosition;
+
+
+        //Direcciones
+
+        if (Input.GetKey(KeyCode.D)) //Si el jugador tiene el boton "D" pulsado 
+        {
+            transform.Translate(Vector2.right * movementSpeed * Time.deltaTime, Space.World); //Se mueve a la derecha
+        }
+
+        if (Input.GetKey(KeyCode.A)) //Si el jugador tiene el boton "A" pulsado 
+        {
+            transform.Translate(Vector2.left * movementSpeed * Time.deltaTime, Space.World); //Se mueve hacia la izquierda
+        }
+
+        if (Input.GetKey(KeyCode.W)) //Si el jugador tiene el boton "W" pulsado 
+        {
+            transform.Translate(Vector2.up * movementSpeed * Time.deltaTime, Space.World); //Se mueve hacia arriba
+        }
+
+        if (Input.GetKey(KeyCode.S)) //Si el jugador tiene el boton "S" pulsado 
+        {
+            transform.Translate(Vector2.down * movementSpeed * Time.deltaTime, Space.World); //Se mueve hacia abajo
+        }
+
+        //Rotacion 
+ 
     }
 }
