1c52aa301ec532d731b63eb747d19f29ef32f0e7
Aaronhdez <aaronhdezalv@hotmail.com>
2022-07-15 20:47:07 +0100

    green: Life check node added

---
+++ Assets/Scripts/AI/Enemies/Nodes/Checks/CheckIfHealthIsZero.cs
@@ -0,0 +1,23 @@
+using BehaviorTree;
+using UnityEngine;
+
+public class CheckIfHealthIsZero : Node
+{
+    private GameObject _agent;
+    private CharacterController _controller;
+
+    public CheckIfHealthIsZero(GameObject agent) {
+        _agent = agent;
+        _controller = agent.GetComponent<CharacterController>();
+    }
+
+    public override NodeState Evaluate() {
+        if (_controller.CharacterLife <= 0) {
+            state = NodeState.SUCCESS;
+            return state;
+        }
+
+        state = NodeState.FAILURE;
+        return state;
+    }
+}
