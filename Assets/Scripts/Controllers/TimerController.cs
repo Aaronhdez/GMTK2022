a0007ad4d5a32a2e3d308b71bcf87e16b42a9151
Aaronhdez <aaronhdezalv@hotmail.com>
2022-07-16 11:27:03 +0100

    green: Basic timer and pause system added

---
+++ Assets/Scripts/Controllers/TimerController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour {
+
+    [Header("Parameters")]
+    [SerializeField] private TextMeshProUGUI _timerTextField;
+    [SerializeField] private string _currentTime;
+    [SerializeField] private float _time = 0;
+    private bool _enabled = false;
+
+    public bool Enabled { get => _enabled; set => _enabled = value; }
+
+    public void StartTimer()
+    {
+        Resume();
+        //_timerTextField.SetText("00:00");
+    }
+
+    void Update() {
+        if (Enabled) { 
+            _time += Time.deltaTime;
+            TimeSpan timeSpan = TimeSpan.FromSeconds(_time);
+            _currentTime = string.Format("{0:D2}:{1:D2}", timeSpan.Minutes, timeSpan.Seconds);
+        }
+    }
+
+    internal void Resume() {
+        Enabled = true;
+    }
+
+    internal void Pause() {
+        Enabled = false;
+    }
+}
