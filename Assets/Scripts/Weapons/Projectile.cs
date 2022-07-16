3f1b5471443280cf6878caaa3ea29f12a6c9c68e
luiss <luiss@DESKTOP-7S4BC9I>
2022-07-16 12:15:46 +0100

    Enemy Dice Update 0.1

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
 
 public abstract class Projectile : MonoBehaviour
 {
     public int damage = 1;
-
     public float speed = 3f;
-
+    public MC_Enemies Dice;
     private void Start()
     {
         Destroy(gameObject, 20);
@@ -23,26 +22,143 @@ public abstract class Projectile : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // TODO: Check active tag on dice
-        if (collision.transform.CompareTag("orc"))
+        switch (MC_Enemies.instance.CurrentEnemy)
         {
-            Attack(collision.gameObject);
+            case 0:
+            if (collision.transform.CompareTag("orc"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 1:
+            if (collision.transform.CompareTag("skeleton"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 2:
+                if (collision.transform.CompareTag("beast"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 3:
+                if (collision.transform.CompareTag("witch"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 4:
+                if (collision.transform.CompareTag("ogger"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 5:
+                if (collision.transform)
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+             default:
+                break;
+
         }
-        else
-        {
-            Destroy(gameObject);
-        }
+        
     }
+       
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // TODO: Check active tag on dice
-        if (collision.transform.CompareTag("orc"))
+
+        switch (MC_Enemies.instance.CurrentEnemy)
         {
-            Attack(collision.gameObject);
+            case 0:
+                if (collision.transform.CompareTag("orc"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 1:
+                if (collision.transform.CompareTag("skeleton"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 2:
+                if (collision.transform.CompareTag("beast"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 3:
+                if (collision.transform.CompareTag("witch"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 4:
+                if (collision.transform.CompareTag("ogger"))
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            case 5:
+                if (collision.transform)
+                {
+                    Attack(collision.gameObject);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+                break;
+            default:
+                break;
+                }
+            }
         }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-}
